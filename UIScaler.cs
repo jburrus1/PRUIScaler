0480297972b620e6ab4a59aa0a6ace149fd264cc
Jack Burrus <jackwburrus@gmail.com>
2024-08-25 14:38:55 -0500

    More menus

--- UIScaler.cs
+++ UIScaler.cs
@@ -61,25 +61,32 @@ namespace PRUIScaler;
                 }
                 foreach (var text in _textToScale[scene].Keys.ToList())
                 {
-                    if (text.text.Length > 0)
+                    try
                     {
-                        _textToScale[scene][text].Invoke(text);
-                        _textToScale[scene].Remove(text);
+                        if (text.text.Length > 0)
+                        {
+                            _textToScale[scene][text].Invoke(text);
+                            _textToScale[scene].Remove(text);
+                        }
                     }
-                }
-            }
-            foreach(var scene in _textToScale.Keys.ToList())
-            {
-                if (!scene.isLoaded)
-                {
-                    continue;
+                    catch
+                    {
+                        LogError($"Could not scale text for {scene.name}.{text?.name ?? "null"}");
+                    }
                 }
                 foreach (var image in _imageToScale[scene].Keys.ToList())
                 {
-                    if ((image.activeSprite.name.Length > 0) && image.gameObject.active)
+                    try
                     {
-                        _imageToScale[scene][image].Invoke(image);
-                        _imageToScale[scene].Remove(image);
+                        if ((image.activeSprite.name.Length > 0) && image.gameObject.active)
+                        {
+                            _imageToScale[scene][image].Invoke(image);
+                            _imageToScale[scene].Remove(image);
+                        }
+                    }
+                    catch
+                    {
+                        LogError($"Could not scale image for {scene.name}.{image?.name ?? "null"}");
                     }
                 }
             }
@@ -93,6 +100,9 @@ namespace PRUIScaler;
         private static void Log(string message)
         {
             // Plugin.Instance.Log.LogInfo(message);
+        }private static void LogError(string message)
+        {
+            Plugin.Instance.Log.LogError(message);
         }
 
         void HandleScene(Scene scene)
@@ -105,8 +115,14 @@ namespace PRUIScaler;
                 case "CampMenu":
                     StartCoroutine(InitCampMenu(scene,1).WrapToIl2Cpp());
                     break;
+                case "MessageWindow":
+                    StartCoroutine(InitMessageWindow(scene, 1).WrapToIl2Cpp());
+                    break;
                 case "TitleScreen":
                     StartCoroutine(InitTitleScreen(scene, 1).WrapToIl2Cpp());
+                    break;
+                case "BattleMenu":
+                    StartCoroutine(InitBattleMenu(scene, 1).WrapToIl2Cpp());
                     break;
                 default:
                     break;
@@ -156,24 +172,76 @@ namespace PRUIScaler;
             yield return null;
         }
 
+        private IEnumerator InitMessageWindow(Scene scene, float textDelay)
+        {
+
+            yield return new WaitForSeconds(textDelay);
+            // Plugin.Instance.Log.LogInfo("Found scene");
+            var rootObjects = scene.GetRootGameObjects().ToList();
+
+            foreach (var root in rootObjects)
+            {
+                for (var i = 0; i < root.transform.childCount; i++)
+                {
+                    var child = root.transform.GetChild(i);
+                    child.transform.localScale = new Vector3(1, scaleRatio, 1);
+                    ScalerHelper(scene,child.transform);
+                }
+            }
+
+            yield return null;
+        }
+
+        private IEnumerator InitBattleMenu(Scene scene, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            var rootGameObjects = scene.GetRootGameObjects();
+            foreach (var root in rootGameObjects)
+            {
+                var relChild = root.transform.Find("Canvas")?.Find("aspect_parent")?.Find("notch_parent")
+                    ?.Find("KeyParent")?.Find("menu_parent")?.Find("menu_parent(Clone)");
+                if (relChild is null)
+                {
+                    LogError($"Could not add scale info for {scene.name}.{root.name}");
+                    continue;
+                }
+                for (var i = 0; i < relChild.transform.childCount; i++)
+                {
+                    var child = relChild.transform.GetChild(i);
+                    child.transform.localScale = new Vector3(1, scaleRatio, 1);
+                    ScalerHelper(scene,child.transform);
+                }
+
+            }
+        }
+
         private void ScalerHelper(Scene scene,Transform child)
         {
-            for (var i = 0; i < child.transform.childCount; i++)
+            try
             {
-                ScalerHelper(scene,child.transform.GetChild(i));
+                for (var i = 0; i < child.transform.childCount; i++)
+                {
+                    ScalerHelper(scene, child.transform.GetChild(i));
+                }
+
+                var text = child.GetComponent<Text>();
+                if (!(text is null))
+                {
+                    // Log($"Scaling text {text.text}");
+                    _textToScale[scene].Add(text, TextScale);
+                }
+
+                var image = child.GetComponent<Image>();
+                if (!(image is null) && !(image.activeSprite is null))
+                {
+                    _imageToScale[scene].Add(image, SpriteScale);
+                }
             }
 
-            var text = child.GetComponent<Text>();
-            if (!(text is null))
+            catch
             {
-                Log($"Scaling text {text.text}");
-                _textToScale[scene].Add(text,TextScale);
-            }
-
-            var image = child.GetComponent<Image>();
-            if (!(image is null) && !(image.activeSprite is null))
-            {
-                _imageToScale[scene].Add(image,SpriteScale);
+                LogError($"Could not add scale info for {scene.name}.{child.name}");
             }
         }
 
@@ -193,8 +261,9 @@ namespace PRUIScaler;
         private void SpriteScale(Image image)
         {
             var spriteTrans = image.transform;
-            Log($"Scaling sprite {image.activeSprite.name}?");
-            if ((!(image.activeSprite.name.Contains("Common") || spriteTrans.name.Contains("Viewport")) ||
+            Log($"Scaling {spriteTrans.name} with sprite {image.activeSprite.name}?");
+            if ((!(image.activeSprite.name.Contains("Common") || spriteTrans.name.Contains("Viewport") ||
+                   image.activeSprite.name.Contains("Background")) ||
                  spriteTrans.name.Contains("Icon")))
             {
                 Log($"Yes");
@@ -203,7 +272,7 @@ namespace PRUIScaler;
                 for (var i = 0; i < spriteTrans.transform.childCount; i++)
                 {
                     var grandchild = spriteTrans.GetChild(i);
-                    grandchild.localScale = new Vector3(grandchild.localScale.x/scaleRatio, 1, 1);
+                    grandchild.localScale = new Vector3(grandchild.localScale.x / scaleRatio, 1, 1);
                 }
             }
             else
--- obj/Debug/netstandard2.1/PluginInfo.cs
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -2,8 +2,8 @@ namespace PRUIScaler
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "PRUIil2cpp";
-        public const string PLUGIN_NAME = "PRUIil2cpp";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_GUID = "PRUIScaler";
+        public const string PLUGIN_NAME = "PRUIScaler";
+        public const string PLUGIN_VERSION = "0.1.0";
     }
 }
